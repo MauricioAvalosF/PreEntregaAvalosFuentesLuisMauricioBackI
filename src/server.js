c0901c7e9df777df6d5d5fff84e98a9b229d7b6a
elmavalos <luismuaaf@gmail.com>
2024-11-29 20:22:21 -0600

    Cambios para la segunda pre-entrega

--- src/server.js
+++ src/server.js
@@ -1,19 +1,57 @@
 import express from 'express'
-import productRouter from './routes/productos.js'
+import { create } from 'express-handlebars'
+import { Server } from 'socket.io'
+import path from 'path'
 import { __dirname } from './path.js'
-import cartRouter from './routes/cart.js'
+import productRouter from './routes/productos.routes.js'
+import cartRouter from './routes/cart.routes.js'
+import chatRouter from './routes/chat.routes.js'
 
 const app = express()
+const hbs = create()
 const PORT = 8080
 
-app.use(express.json())
+const server = app.listen(PORT, () => {
+    console.log("Server on port", PORT)
+})
+
+//Inicializo Socket.io en el servidor
+const io = new Server(server)
+//Middlewares de aplicacion
+app.use(express.json()) //Para manejar JSON en las peticiones
 app.use(express.urlencoded({extended: true}))
+//Configuracion de hbs para localizacion de plantillas y extension de archivo
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
 
-app.use('/static', express.static(__dirname +'/public'))
+//Establezco el directorio de las vistas
+app.set('views', path.join(__dirname, 'views'))
 
-app.use('/api/carts', cartRouter)
+//Rutas de mi aplicacion
+app.use('/public', express.static(__dirname + '/public'))//Defino la carpeta publica como destino de los archivos estaticos
 app.use('/api/products', productRouter)
+app.use('/api/carts', cartRouter)
+app.use('/api/chat', chatRouter)
+app.get('/', (req,res) => {
+    res.status(200).send("ok")
+})
 
-app.listen(PORT, () => {
-    console.log("Servidor on port", PORT)
+let mensajes = []
+//Conexiones de socket.io
+//socket = info que llega de la conexion
+io.on('connection', (socket) => { //Cuando se producza el "apreton de manos", puedo ejecutar las sigueintes funciones
+    console.log('Usuario conectado: ', socket.id); //ID de conexion
+    
+    socket.on('mensaje', (data) => { //Cuando el usuario me envia un mensaje, trabajo con esos datos
+        console.log('Mensaje recibido: ', data);
+        mensajes.push(data)
+        //Envia el array de mensajes
+        socket.emit('respuesta', mensajes)
+    })
+
+    //Detectar desconexion
+    socket.on('disconnect', ()=> {
+        console.log('Usuario desconectado: ', socket.id);
+        
+    })
 })
